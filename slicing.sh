752b17a957f42ce46541060ca4950ffdf019d550
DanielMwandiki <danmasketcha@gmail.com>
2022-08-11 20:02:17 +0300

    bash scripts

---
+++ slicing.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+letters=("A""B""C""D""E")
+
+#This command will print all elements of an array
+#echo ${letters[@]}
+
+#This command will print array from starting index 0 to 2 where 2 is 
+#exclusive.
+#b=${letters:0:2}
+#echo "${b}"
+
+#This command will print from base index 0 to 5, where 5 is 
+#exclusive and starting index is default set to 0 .
+#b=${letters::5}
+#echo "${b}"
+
+#This command will print from starting index 3 to end of array inclusive .
+b=${letters:3}
+echo "${b}"
